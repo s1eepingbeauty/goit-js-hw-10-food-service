d3cdde475793c4b66597da5ffe2ab92101dcd499
Elena <mazurenko_elena@i.ua>
2020-12-13 14:03:05 +0200

    added markup

--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
 import './styles.css';
+import menuData from './menu.json';
+import menuTemplate from './templates/menu.hbs';
+
+const markup = menuTemplate(menuData);
+const ulRef = document.querySelector('.js-menu');
+ulRef.insertAdjacentHTML('beforeend', markup);
 
 const Theme = {
   LIGHT: 'light-theme',
