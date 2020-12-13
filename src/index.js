9fbb7b2d1e540054cf621ad350424360003a9484
Elena <mazurenko_elena@i.ua>
2020-12-14 01:57:24 +0200

    added tamplate & toggle stuff

--- src/index.js
+++ src/index.js
@@ -2,11 +2,33 @@ import './styles.css';
 import menuData from './menu.json';
 import menuTemplate from './templates/menu.hbs';
 
-const markup = menuTemplate(menuData);
-const ulRef = document.querySelector('.js-menu');
-ulRef.insertAdjacentHTML('beforeend', markup);
-
 const Theme = {
   LIGHT: 'light-theme',
   DARK: 'dark-theme',
 };
+const bodyRef = document.querySelector('body');
+const toggleRef = document.querySelector('#theme-switch-toggle');
+const ulRef = document.querySelector('.js-menu');
+
+const markup = menuTemplate(menuData);
+ulRef.insertAdjacentHTML('beforeend', markup);
+
+const settings = localStorage.getItem('settings');
+bodyRef.classList.add(settings);
+
+if (settings === 'dark-theme') {
+  toggleRef.checked = true;
+}
+
+function changeTheme() {
+  bodyRef.className = '';
+  if (toggleRef.checked) {
+    bodyRef.classList.add(Theme.DARK);
+    localStorage.setItem('settings', Theme.DARK);
+  } else {
+    bodyRef.classList.add(Theme.LIGHT);
+    localStorage.setItem('settings', Theme.LIGHT);
+  }
+}
+
+toggleRef.addEventListener('change', changeTheme);
